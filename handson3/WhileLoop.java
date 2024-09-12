5b25f23016cd6c3293bdfdad2dd71b682cd218e6
Arvind <2381040@unai.edu>
2024-09-12 10:37:36 +0700

    handson3

---
+++ handson3/WhileLoop.java
@@ -0,0 +1,17 @@
+package handson3;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 20) {
+            if (i > 15) {
+                break;
+            }
+            if (i > 15) {
+                continue;
+            }
+            System.out.println(i);
+            i++;
+        }
+    }
+}
